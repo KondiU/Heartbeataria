c790f219d4a7ab6f4489980be65acc2b4158c664
KondiU <kondi412@gmail.com>
2023-01-16 22:11:36 +0100

    Added TV & a folder for currently unused sprites from Chinese versions

---
+++ Content/Items/Placeable/TV.cs
@@ -0,0 +1,45 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace XDContentMod.Content.Items.Placeable
+{
+	public class TV : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("TV");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 44;
+			Item.height = 25;
+			Item.rare = ItemRarityID.White;
+			Item.value = Item.buyPrice(silver: 100);
+
+			Item.maxStack = 99;
+
+			Item.useStyle = 1;
+			Item.useTime = 10;
+			Item.useAnimation = 15;
+
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.consumable = true;
+
+			Item.createTile = ModContent.TileType<Content.Tiles.Furniture.TVTile>();
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe(1);
+			recipe.AddRecipeGroup(RecipeGroupID.Wood, 12);
+			recipe.AddIngredient(ItemID.Glass, 10);
+			recipe.AddIngredient(ItemID.Wire, 10);
+			recipe.AddTile(TileID.Sawmill);
+			recipe.Register();
+		}
+
+	}
+}
---
+++ Content/Tiles/Furniture/TVTile.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace XDContentMod.Content.Tiles.Furniture
+{
+    public class TVTile : ModTile
+    {
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolidTop[Type] = false;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileTable[Type] = false;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("TV");
+            AddMapEntry(new Color(99, 99, 99), name);
+			TileID.Sets.DisableSmartCursor[Type] = true;
+		}
+
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Terraria.Item.NewItem(new Terraria.DataStructures.EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 16, ModContent.ItemType<Content.Items.Placeable.TV>());
+		}
+	}
+}
