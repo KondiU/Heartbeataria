b54818ce08a212a09ce738ceea0be7625bfccf2a
KondiU <kondi412@gmail.com>
2023-04-11 10:51:08 +0200

    Dormant Dawn Mod fix
    -Fixed a conflict with Dormant Dawn Mod

--- Content/Items/Weapons/Melee/HeartbeatBroadsword.cs
+++ Content/Items/Weapons/Melee/HeartbeatBroadsword.cs
@@ -14,8 +14,18 @@ namespace XDContentMod.Content.Items.Weapons.Melee
 
 		public override void SetDefaults()
 		{
-			Item.CloneDefaults(ItemID.PlatinumBroadsword);
-			Item.rare = ItemRarityID.White;
+			Item.damage = 16;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 24;
+			Item.height = 28;
+			Item.useTime = 17;
+			Item.useAnimation = 17;
+			Item.useStyle = 1;
+			Item.knockBack = 6.5;
+			Item.value = Item.buyPrice(silver: 135);
+			Item.rare = 0;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = false;
 		}
 	}
 }
