a065172c2ac4e5d9fc6f64cf4ca97f11feeedcbc
KondiU <kondi412@gmail.com>
2023-05-15 22:10:37 +0200

    Update VanillaNPCShop.cs

--- Common/GlobalNPC/VanillaNPCShop.cs
+++ Common/GlobalNPC/VanillaNPCShop.cs
@@ -16,14 +16,6 @@ namespace XDContentMod.Common
 					shop.Add<ImperialMasterBreastplate>(Condition.DownedMoonLord, Condition.MoonPhaseFull);
                     shop.Add<ImperialMasterLeggings>(Condition.DownedMoonLord, Condition.MoonPhaseFull);
 
-					shop.Add<GuaPiMao>(Condition.DownedEowOrBoc, Condition.MoonPhaseWaningGibbous);
-					shop.Add<TangSuitShirt>(Condition.DownedEowOrBoc, Condition.MoonPhaseWaningGibbous);
-                    shop.Add<TangSuitPants>(Condition.DownedEowOrBoc, Condition.MoonPhaseWaningGibbous);
-
-                    shop.Add<MythicalDogMask>(Condition.Hardmode, Condition.MoonPhaseThirdQuarter);
-					shop.Add<MythicalDogShirt>(Condition.Hardmode, Condition.MoonPhaseThirdQuarter);
-                    shop.Add<MythicalDogPants>(Condition.Hardmode, Condition.MoonPhaseThirdQuarter);
-
 					shop.Add<ImperialGuardHelmet>(Condition.DownedSkeletron, Condition.MoonPhaseWaningCrescent);
 					shop.Add<ImperialGuardBreastplate>(Condition.DownedSkeletron, Condition.MoonPhaseWaningCrescent);
                     shop.Add<ImperialGuardLeggings>(Condition.DownedSkeletron, Condition.MoonPhaseWaningCrescent);
@@ -35,10 +27,6 @@ namespace XDContentMod.Common
 					shop.Add<DeepColdHelmet>(Condition.DownedIceQueen, Condition.MoonPhaseFirstQuarter);
 					shop.Add<DeepColdBreastplate>(Condition.DownedIceQueen, Condition.MoonPhaseFirstQuarter);
                     shop.Add<DeepColdLeggings>(Condition.DownedIceQueen, Condition.MoonPhaseFirstQuarter);
-
-					shop.Add<TangYuanHat>(Condition.DownedPirates, Condition.MoonPhaseWaxingGibbous);
-					shop.Add<TangYuanShirt>(Condition.DownedPirates, Condition.MoonPhaseWaxingGibbous);
-                    shop.Add<TangYuanPants>(Condition.DownedPirates, Condition.MoonPhaseWaxingGibbous);
             }
         }
     }
