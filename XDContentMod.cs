6fd3e108e7370a42c75735b77b54309a9ec1ede0
CgAu3 <2427310499@qq.com>
2023-12-28 16:09:30 +0800

    Introduced Chinese texts and ShopLookup Support. See Changelog.

--- XDContentMod.cs
+++ XDContentMod.cs
@@ -1,8 +1,17 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace XDContentMod
 {
 	public class XDContentMod : Mod
 	{
-	}
+        public override void PostSetupContent()
+        {
+            if (ModLoader.TryGetMod("ShopLookup", out Mod slu))
+            {
+                slu.Call("NonPermanent", ModContent.NPCType<Content.NPCs.StarMerchantNPC>(), Condition.TimeDay);
+            }
+            base.PostSetupContent();
+        }
+    }
 }
